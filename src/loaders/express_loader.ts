1e7bf0941c593b352dd2e3a0cd0ab16178cfe45e
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-15 18:14:27 +0200

    setup all routes

--- src/loaders/express_loader.ts
+++ src/loaders/express_loader.ts
@@ -1,18 +1,18 @@
 import { Application } from "express";
 import { json } from "body-parser";
 import cors from "cors";
-import * as authRoutes from "../routes/auth_utils";
-import * as userRoutes from "../routes/user_utils";
+import * as authRoute from "../routes/auth_routes";
+import * as userRoute from "../routes/user_routes";
 import { verifyToken } from "../middlewares/verify_token";
 const expressLoader = (app: Application) => {
   app.use(json());
   app.use(cors());
 
-  app.post("/register", authRoutes.register);
-  app.post("/login", authRoutes.login);
-  app.post("/add_user", userRoutes.addUser);
+  app.post("/register", authRoute.register);
+  app.post("/login", authRoute.login);
+  app.post("/add_user", userRoute.addUser);
 
-  app.get("/my_info", verifyToken, userRoutes.myInfo);
+  app.get("/my_info", verifyToken, userRoute.myInfo);
 };
 
 export { expressLoader };
